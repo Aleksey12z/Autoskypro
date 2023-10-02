b90b91e7da51bdd78643dfb951ecfaaabe1f2a7f
Алексей Кудряшов <129525516+Aleksey12z@users.noreply.github.com>
2023-10-02 23:08:11 +0300

    Update lesson_1_task_4.py

--- Домашка1/lesson_1_task_4.py
+++ Домашка1/lesson_1_task_4.py
@@ -1,4 +1,3 @@
-first_name = ("Алексей ")
-last_name = ("Кудряшов ")
-
-print( last_name, first_name)
+first_name = input("Введите свое имя: ")
+last_name = input("Введите свою фамилию: ")
+print("Вас зовут:", last_name, first_name)
